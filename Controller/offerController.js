ce67429c25db068b74812577cc772feaac4a8d45
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-09-12 16:59:31 +0530

    offer to user by vendor complted

--- Controller/offerController.js
+++ Controller/offerController.js
@@ -21,11 +21,13 @@ const addOffer = async (req,res)=>{
         console.log("input : ",req.body.input)
         const offerData = req.body.input
         console.log(req.id)
+        const oneTime = offerData.oneTime === 'true' ? true : false;
         const addOffer = new Offer({
             vendor:req.id,
             offerName:offerData.offerName,
             percentage:offerData.percentage,
-            description:offerData.description
+            description:offerData.description,
+            oneTime:oneTime
         })
         await addOffer.save()
         res.status(200).json({success:true})
@@ -34,7 +36,58 @@ const addOffer = async (req,res)=>{
     }
 }
 
+// ------------------------------------------------------------ get offer list for vendor --------------------------------------------------------
+
+const getOfferstoVendor = async (req,res) =>{
+    try {
+        console.log("entered offerLists ")
+        const partnerId = req.id
+        console.log(partnerId)
+        const offers = await Offer.find({vendor:partnerId})
+        console.log("offers : : : ",offers)
+        res.status(200).json({success:true,offers})
+    } catch (error) {
+        console.log("error in offeradd : ",error.message)
+        
+    }
+}
+// ------------------------------------------------------------ list Offer --------------------------------------------------------
+const listOffer = async (req,res) =>{
+    try {
+        console.log("entered list ")
+        console.log("reabody : ",req.body)
+
+        const partnerId = req.id
+        console.log(partnerId)
+        const offers = await Offer.updateOne({_id:req.body.id},{$set:{isListed:true}})
+        console.log("offers : : : ",offers)
+        res.status(200).json({success:true,offers})
+    } catch (error) {
+        console.log("error in offeradd : ",error.message)
+    }
+}
+
+// ------------------------------------------------------------ unlist Offer --------------------------------------------------------
+
+const unlistOffer = async (req,res) =>{
+    try {
+        console.log("entered unlist--------------------- ")
+        console.log("reabody : ",req.body)
+        const partnerId = req.id
+        console.log(partnerId)
+        const offers = await Offer.updateOne({_id:req.body.id},{$set:{isListed:false}})
+        console.log("offers : : : ",offers)
+
+        res.status(200).json({success:true,offers})
+    } catch (error) {
+        console.log("error in offeradd : ",error.message)
+        
+    }
+}
 module.exports = {
     addOffer,
-    getOffers
+    getOffers,
+    getOfferstoVendor,
+    listOffer,
+    unlistOffer
 }
